6736916163d8f5f47d3e1c20b2eec8fa5205d7cd
harsh-1712 <harsh.tiwari603@gmail.com>
2021-09-07 21:32:52 +0530

    added

---
+++ C++ PROGRAMS/quardraric.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, real, img, D;
+    cout << "Enter numbers : ";
+    cin >> a >> b >> c;
+    D=b*b-(4*a*c);
+    if(D>0)
+    {
+        
+    }
+    return 0;
+}
